2aef42613f3a363ce310674a7d8ebec8ca1e0535
jakeolson613 <144736103+jakeolson613@users.noreply.github.com>
2025-07-23 08:40:44 -0500

    Added Axure RP 11

--- Installomator.sh
+++ Installomator.sh
@@ -2502,6 +2502,18 @@ axurerp10)
     expectedTeamID="HUMW6UU796"
     versionKey="CFBundleVersion"
     ;;
+axurerp11)
+    name="Axure RP 11"
+    type="dmg"
+    if [[ $(arch) = "arm64" ]]; then
+        printlog "Architecture: arm64"
+        downloadURL="https://d3uii9pxdigrx1.cloudfront.net/AxureRP-Setup-arm64.dmg"
+    else
+        downloadURL="https://d3uii9pxdigrx1.cloudfront.net/AxureRP-Setup.dmg"
+    fi
+    expectedTeamID="HUMW6UU796"
+    versionKey="CFBundleVersion"
+    ;;
 backgroundmusic)
     name="BackgroundMusic"
     type="pkg"
